bd415d0acc5d53c968fb9edc17bc437f35a9631f
Ryagthebest <jeanmidlerrya88@gmail.com>
2025-01-03 12:57:01 -0300

    tentativa de correçaõ exe 005

--- exe005/script.js
+++ exe005/script.js
@@ -1,22 +1,19 @@
 function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    //var hora = 20
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem'); // Certifique-se de que o ID é "imagem"
+    var data = new Date();
+    // var hora = data.getHours();
+    var hora = 20; // Hora de teste
+    msg.innerHTML = `Agora são ${hora} horas.`;
 
-    if (hora >= 0 && hora < 12) {
-        // bom dia 
-        img.src = 'imagemmanha.png' 
-        document.body.style.background = '#f7e2ce'
-    } else if (hora >= 12 && hora < 18) {
-        // boa tarde 
-        img.src = 'imagemtarde.png' 
-        document.body.style.background = '#e1a95f'
-    } else {
-        // boa noite
-        img.src = 'imagemnoite2.png' 
-        document.body.style.background = '#131b1d'
+    if (hora >= 0 && hora < 12) { // Bom dia
+        img.src = img; // Certifique-se do caminho
+        document.body.style.background = '#f7e2ce';
+    } else if (hora >= 12 && hora < 18) { // Boa tarde
+        img.src = `${img}`; // Corrigido o caminho
+        document.body.style.background = '#e1a95f';
+    } else { // Boa noite
+        img.src = 'imagens/imagemnoite2.png'; // Certifique-se do caminho
+        document.body.style.background = '#131b1d';
     }
 }

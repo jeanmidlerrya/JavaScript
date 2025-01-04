fc021ad64becab3c3e8ece272d21836c3b382135
Ryagthebest <jeanmidlerrya88@gmail.com>
2025-01-04 12:02:45 -0300

    corrigir o error exe005

--- exe005/script.js
+++ exe005/script.js
@@ -2,15 +2,15 @@ function carregar() {
     var msg = window.document.getElementById('msg');
     var img = window.document.getElementById('imagem'); // Certifique-se de que o ID é "imagem"
     var data = new Date();
-    // var hora = data.getHours();
-    var hora = 20; // Hora de teste
+    var hora = data.getHours();
+    //var hora =18 ; // Hora de teste
     msg.innerHTML = `Agora são ${hora} horas.`;
 
     if (hora >= 0 && hora < 12) { // Bom dia
-        img.src = img; // Certifique-se do caminho
+        img.src = 'imagens/imagemmanha.png'; // Certifique-se do caminho
         document.body.style.background = '#f7e2ce';
     } else if (hora >= 12 && hora < 18) { // Boa tarde
-        img.src = `${img}`; // Corrigido o caminho
+        img.src = 'imagens/imagemtarde.png'; // Corrigido o caminho
         document.body.style.background = '#e1a95f';
     } else { // Boa noite
         img.src = 'imagens/imagemnoite2.png'; // Certifique-se do caminho
